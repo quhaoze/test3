55a00b60fe375995ead6f0ab44de7d67c2ee5cc3
quhaoze <quhaoze110110@163.com>
2024-04-05 23:12:01 +0800

    流水灯

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -98,6 +98,14 @@ int main(void)
     /* USER CODE BEGIN 3 */
       HAL_Delay(50);
       HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_1);
+      HAL_Delay(50);
+      HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_2);
+      HAL_Delay(50);
+      HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_3);
+      HAL_Delay(50);
+      HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_4);
+      HAL_Delay(50);
+      HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
   }
   /* USER CODE END 3 */
 }
@@ -157,10 +165,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PA1 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  /*Configure GPIO pins : PA1 PA2 PA3 PA4
+                           PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
+                          |GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
